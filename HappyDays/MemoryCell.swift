eff64182594abbf3a4098f88706d4bd9901e3b67
Charles Findlay <charlesfindlay2000-coding@yahoo.com>
2016-09-10 19:39:42 -0400

    Added skeleton of class files

---
+++ HappyDays/MemoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemoryCell.swift
+//  HappyDays
+//
+//  Created by Charles Findlay on 9/10/16.
+//  Copyright © 2016 Charles Findlay. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+}
