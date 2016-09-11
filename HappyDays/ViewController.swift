5473cb02eacb06e5545e343f5b915b2d53b7bc16
Charles Findlay <charlesfindlay2000-coding@yahoo.com>
2016-09-10 22:33:26 -0400

    Added permission functions for photos, speech, and transcription

--- HappyDays/ViewController.swift
+++ HappyDays/ViewController.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
+
 
 class ViewController: UIViewController {
 
@@ -23,7 +27,70 @@ class ViewController: UIViewController {
     }
 
     @IBAction func requestPermissions(_ sender: AnyObject) {
+        
+        requestPhotoPermissions()
+        
     }
+    
+    func requestPhotoPermissions() {
+        
+        PHPhotoLibrary.requestAuthorization { [unowned self] authStatus in
+            
+            DispatchQueue.main.async {
+                
+                if authStatus == .authorized {
+                    
+                    self.requestRecordPermissions()
+                } else {
+                    
+                    self.helpLabel.text="Photo permission was declined. Please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    } // End requestPhotoPermissions
+    
+    
+    func requestRecordPermissions() {
+        
+        AVAudioSession.sharedInstance().requestRecordPermission() { [unowned self] allowed in
+            
+            DispatchQueue.main.async {
+                if allowed {
+                    
+                    self.requestTranscribePermissions()
+                } else {
+                    
+                    self.helpLabel.text="Recording permission was declined. Please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    } // End requestRecordPermissions
+    
+    
+    func requestTranscribePermissions() {
+        
+        SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
+            
+            DispatchQueue.main.async {
+                
+                if authStatus == .authorized {
+                    
+                    self.authorizationComplete()
+                } else {
+                    
+                    self.helpLabel.text="Transcribe permission was declined. Please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    } // End requestTranscribePermissions
+    
+    func authorizationComplete() {
+        
+        dismiss(animated: true)
+    }
+    
+    
+    
 
 }
 
